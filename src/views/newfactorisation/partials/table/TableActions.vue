6f02ff561557feed81432f30c795b94e2b074a77
Abdelmounaim <oulaidabdelmonaim2@gmail.com>
2023-11-01 18:52:56 +0100

    changes factures

--- src/views/newfactorisation/partials/table/TableActions.vue
+++ src/views/newfactorisation/partials/table/TableActions.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="tw-h-full">
     <div class="tw-flex tw-items-center tw-gap-2">
-    <button v-if="$user.role == 'admin'" @click="CommentPopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-blue-500/20 hover:tw-bg-blue-500/10 hover:tw-border-blue-500/70 tw-duration-300 tw-text-blue-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
+    <v-btn v-if="$user.role == 'admin'" target="_blank" :href="serverUrl + 'api/export/factorisations/' + item.factorisation_id" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-green-500/20 hover:tw-bg-green-500/10 hover:tw-border-green-500/70 tw-duration-300 tw-text-green-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
+        <v-icon size="x-small" >mdi-microsoft-excel</v-icon>
+    </v-btn>
+    
+    <v-btn v-if="$user.role == 'admin'" @click="CommentPopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-blue-500/20 hover:tw-bg-blue-500/10 hover:tw-border-blue-500/70 tw-duration-300 tw-text-blue-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
         <v-icon size="x-small" >mdi-comment-text</v-icon>
-    </button>
-    <button v-if="$user.role == 'admin'" @click="popup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-orange-500/20 hover:tw-bg-orange-500/10 hover:tw-border-orange-500/70 tw-duration-300 tw-text-orange-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
+    </v-btn>
+    <v-btn v-if="$user.role == 'admin'" @click="popup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-orange-500/20 hover:tw-bg-orange-500/10 hover:tw-border-orange-500/70 tw-duration-300 tw-text-orange-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
         <v-icon size="x-small" >mdi-pencil-outline</v-icon>
-    </button>
-    <button  v-if="$user.role == 'admin'" @click="DeletePopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-red-500/20 hover:tw-bg-red-500/10 hover:tw-border-red-500/70 tw-duration-300 tw-text-red-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
+    </v-btn>
+    <v-btn  v-if="$user.role == 'admin'" @click="DeletePopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-red-500/20 hover:tw-bg-red-500/10 hover:tw-border-red-500/70 tw-duration-300 tw-text-red-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
         <v-icon size="x-small" >mdi-delete-outline</v-icon>
-    </button>
-    <button v-if="$user.role == 'admin' && item.type =='seller'" @click="FeesPopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-green-500/20 hover:tw-bg-green-500/10 hover:tw-border-green-500/70 tw-duration-300 tw-text-green-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
+    </v-btn>
+    <v-btn v-if="$user.role == 'admin' && item.type =='seller'" @click="FeesPopup = true" class="tw-px-2 tw-py-1 tw-w-[25px] tw-h-[25px] tw-border tw-border-solid tw-border-green-500/20 hover:tw-bg-green-500/10 hover:tw-border-green-500/70 tw-duration-300 tw-text-green-500/80 tw-rounded-md tw-flex tw-items-center tw-justify-center">
         <v-icon size="x-small" >mdi-format-list-text</v-icon>
-    </button>
+    </v-btn>
+  
     </div>
 
     <div v-if="CommentPopup">
@@ -37,6 +42,7 @@
 import CommentFactorisation from './../components/CommentFactorisation';
 import UpdateFactorisation from './../components/UpdateFactorisation';
 import UpdateFees from './../components/UpdateFees';
+import { serverUrl } from '@/config/config';
  
 export default {
     components: { CommentFactorisation, UpdateFactorisation , UpdateFees },
@@ -54,7 +60,7 @@ export default {
             DeletePopup: false,
             FeesPopup:false,
             isLoading: false,
-
+            serverUrl
         }
     },
     computed:{
