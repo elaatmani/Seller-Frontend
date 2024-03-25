3153bae17cc00f12bc3029ff090decbb9f17f721
GG-480_O <omnigold450@gmail.com>
2024-03-25 13:40:41 +0000

    tasks

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 
-export const localUrl = 'https://codsquad.net/'
-export const serverUrl = 'https://api.codsquad.net/'
+// export const localUrl = 'https://codsquad.net/'
+// export const serverUrl = 'https://api.codsquad.net/'
 
 
-// export const localUrl = 'http://localhost:8080/'
-// export const serverUrl = 'http://localhost:8000/'
+export const localUrl = 'http://localhost:8080/'
+export const serverUrl = 'http://localhost:8000/'
 
 export const version = '1.0.1';
 
