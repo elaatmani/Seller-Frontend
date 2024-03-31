a6ef2b6a46e64d81e5868fe197d4b372592c4886
Abdelmonaim <oulaidabdelmonaim2@gmail.com>
2024-03-31 18:02:25 +0000

    fix: sum to be paied

--- src/layouts/default/partials/header/ToBePaid.vue
+++ src/layouts/default/partials/header/ToBePaid.vue
@@ -24,8 +24,8 @@ const gettobepaid = () => {
 Factorisation.sumToBePaid()
     .then((res) => {
     if (res.data.code == "SUCCESS") {
-        store.dispatch("app/settobepaid", res.data.totalPrice);
-        console.log( res.data.totalPrice);
+        store.dispatch("app/settobepaid", parseInt(res.data.data.totalPrice));
+        console.log( res.data.data );
     }
     })
     .catch((error) => {
