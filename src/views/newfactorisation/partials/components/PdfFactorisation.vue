269edbd8cca6825d2173877da9670e2ec63e5f06
Yassine El Aatmani <devyassine47@gmail.com>
2024-05-10 17:33:16 +0100

    added new status cleared & fixed invoice & image in seller

--- src/views/newfactorisation/partials/components/PdfFactorisation.vue
+++ src/views/newfactorisation/partials/components/PdfFactorisation.vue
@@ -4,6 +4,9 @@
     <button class="tw-h-full tw-w-[28px] tw-aspect-square tw-border tw-border-solid tw-shadowx tw-flex tw-items-center tw-text-gray-500 hover:tw-text-gray-7 tw-duration-200 tw-justify-center hover:tw-bg-gray-100 tw-rounded tw-p-0.5" @click="download(item.id)">
       <icon icon="ci:arrow-down-sm" class="tw-text-xl" />
     </button>
+    <a target="_blank" :href="$backend(item.attachement_image.startsWith('/') ? item.attachement_image.substring(1) : item.attachement_image)" v-if="item.close && item.paid & !!item.attachement_image" class="tw-h-full tw-w-[28px] tw-aspect-square tw-border tw-border-solid tw-border-cyan-200 tw-shadowx tw-flex tw-items-center tw-text-gray-500 hover:tw-text-gray-7 tw-duration-200 tw-justify-center hover:tw-bg-gray-100 tw-rounded tw-p-0.5" >
+      <icon icon="uim:image-v" class="tw-text-xl tw-text-cyan-400" />
+    </a>
   </h2>
 </template>
 
--- src/views/newfactorisation/partials/table/TableActions.vue
+++ src/views/newfactorisation/partials/table/TableActions.vue
@@ -10,22 +10,22 @@
         <div v-if="visible" v-click-outside="() => visible = false" class="tw-absolute tw-right-0 tw-z-10">
             <div class="tw-absolute tw-top-1/2 -tw-translate-y-1/2 tw-right-10 tw-bg-white tw-border tw-border-solid  tw-p-2 tw-rounded tw-flex tw-items-center tw-gap-2">
                 
-                <button v-if="$user.role == 'admin'" @click="HistoryPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-teal-100 hover:tw-border-teal-400 tw-bg-teal-50/10 hover:tw-bg-teal-100 tw-duration-200">
+                <button title="Show history" v-if="$user.role == 'admin'" @click="HistoryPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-teal-100 hover:tw-border-teal-400 tw-bg-teal-50/10 hover:tw-bg-teal-100 tw-duration-200">
                     <icon icon="uim:history" class="tw-text-xl tw-text-teal-500" />
                 </button>
                 
-                <a v-if="$user.role == 'admin'" target="_blank" :href="serverUrl + 'api/export/factorisations/' + item.factorisation_id" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-green-100 hover:tw-border-green-400 tw-bg-green-50/10 hover:tw-bg-green-200 tw-duration-200">
+                <a title="Download Excel File" v-if="$user.role == 'admin'" target="_blank" :href="serverUrl + 'api/export/factorisations/' + item.factorisation_id" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-green-100 hover:tw-border-green-400 tw-bg-green-50/10 hover:tw-bg-green-200 tw-duration-200">
                     <icon icon="vscode-icons:file-type-excel" class="tw-text-xl" />
                 </a>
 
-                <button v-if="$user.role == 'admin'" @click="CommentPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-blue-100 hover:tw-border-blue-400 tw-bg-blue-50/10 hover:tw-bg-blue-100 tw-duration-200">
+                <button title="Add comment" v-if="$user.role == 'admin' && false" @click="CommentPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-blue-100 hover:tw-border-blue-400 tw-bg-blue-50/10 hover:tw-bg-blue-100 tw-duration-200">
                     <icon icon="solar:notes-bold-duotone" class="tw-text-xl tw-text-blue-500" />
                 </button>
 
-                <button v-if="$user.role == 'admin' && false" @click="popup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-amber-100 hover:tw-border-amber-400 tw-bg-amber-50/10 hover:tw-bg-amber-100 tw-duration-200">
+                <button title="Edit" v-if="$user.role == 'admin' && false" @click="popup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-amber-100 hover:tw-border-amber-400 tw-bg-amber-50/10 hover:tw-bg-amber-100 tw-duration-200">
                     <icon icon="lets-icons:edit-duotone" class="tw-text-xl tw-text-amber-500" />
                 </button>
-                <button v-if="$user.role == 'admin'" @click="DeletePopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-rose-100 hover:tw-border-rose-400 tw-bg-rose-50/10 hover:tw-bg-rose-100 tw-duration-200">
+                <button title="Delete" v-if="$user.role == 'admin'" @click="DeletePopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-rose-100 hover:tw-border-rose-400 tw-bg-rose-50/10 hover:tw-bg-rose-100 tw-duration-200">
                     <icon icon="solar:trash-bin-trash-bold-duotone" class="tw-text-xl tw-text-rose-500" />
                 </button>
 
@@ -33,11 +33,11 @@
         </div>
     </transition>
 
-    <button v-if="$user.role == 'admin'" @click="FeesPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-emerald-100 hover:tw-border-emerald-400 tw-bg-emerald-50/10 hover:tw-bg-emerald-100 tw-duration-200">
+    <button title="Add fees" v-if="$user.role == 'admin'" @click="FeesPopup = true" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-emerald-100 hover:tw-border-emerald-400 tw-bg-emerald-50/10 hover:tw-bg-emerald-100 tw-duration-200">
         <icon icon="solar:bill-list-bold-duotone" class="tw-text-xl tw-text-emerald-500" />
     </button>
 
-    <TableActionsUploadImage v-if="$user.role == 'admin'" @update="newItem => $emit('update', newItem)" :key="item.id" :item="item" />
+    <TableActionsUploadImage title="Upload image" v-if="$user.role == 'admin'" @update="newItem => $emit('update', newItem)" :key="item.id" :item="item" />
 
     <button @click="visible = !visible" v-if="$user.role == 'admin'" class="tw-aspect-square tw-p-0.5 tw-w-[32px] tw-flex tw-items-center tw-justify-center tw-rounded tw-border tw-border-solid tw-border-gray-100 hover:tw-border-gray-400 tw-bg-gray-50/10 hover:tw-bg-gray-100 tw-duration-200">
         <icon :icon="visible ? 'iconamoon:close-duotone' : 'solar:menu-dots-bold'" class="tw-text-xl tw-text-gray-500" />
